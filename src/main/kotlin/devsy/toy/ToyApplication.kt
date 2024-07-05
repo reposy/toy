ca7669296e3b749b1794090fb08ba92852c2cdae
syhan <hsykys0728@gmail.com>
2024-07-05 22:54:34 +0900

    Add FeignClient

--- src/main/kotlin/devsy/toy/ToyApplication.kt
+++ src/main/kotlin/devsy/toy/ToyApplication.kt
@@ -2,8 +2,10 @@ package devsy.toy
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class ToyApplication
 
 fun main(args: Array<String>) {
