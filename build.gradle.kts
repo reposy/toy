67717f3867023b3bd052a0584a200b9315eb4d8e
syhan <hsykys0728@gmail.com>
2024-07-05 23:03:42 +0900

    Add openfeign

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,14 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.1.2") {
+        exclude(group = "commons-fileupload", module = "commons-fileupload")
+    }
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
