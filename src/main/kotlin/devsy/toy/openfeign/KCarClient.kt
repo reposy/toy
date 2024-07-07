1294f627a1bead9bc744adb2d60f17df9b5bcc99
syhan <hsykys0728@gmail.com>
2024-07-07 22:08:44 +0900

    add kcar getVehicleList, getVehicleDetail

--- src/main/kotlin/devsy/toy/openfeign/KCarClient.kt
+++ src/main/kotlin/devsy/toy/openfeign/KCarClient.kt
@@ -2,10 +2,23 @@ package devsy.toy.openfeign
 
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 
 @FeignClient(name = "kcarClient", url = "https://www.kcar.com")
 interface KCarClient {
 
     @GetMapping("/robots.txt")
-    fun getRobotsTxt(): String
+    fun getRobotsTxt(
+        @RequestHeader("Content-Type") contentType: String = "application/json; charset=utf-8"
+    ): String
+
+    @GetMapping("/bc/search")
+    fun getVehicleList(
+        @RequestHeader("Content-Type") contentType: String = "application/json; charset=utf-8"
+    ): String
+
+    @GetMapping("/bc/detail/carInfoDtl")
+    fun getVehicleDetail(
+        @RequestHeader("Content-Type") contentType: String = "application/json; charset=utf-8"
+    ): String
 }
