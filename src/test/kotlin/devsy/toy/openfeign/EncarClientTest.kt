da9e998203bc93f8259693a097a429068b52151b
syhan <hsykys0728@gmail.com>
2024-07-07 22:11:10 +0900

    add encarClientTest, KcarClientTest - getRobotsTxtTest()

--- src/test/kotlin/devsy/toy/openfeign/EncarClientTest.kt
+++ src/test/kotlin/devsy/toy/openfeign/EncarClientTest.kt
@@ -1,19 +1,27 @@
 package devsy.toy.openfeign
 
+import devsy.toy.util.CrawlerUtil
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class EncarClientTest {
-    @Autowired
-    private lateinit var encarClient: EncarClient
+class EncarClientTest(
+    @Autowired val encarClient: EncarClient,
+    @Autowired val crawlerUtil: CrawlerUtil
+) {
 
     @Test
-    fun getRobotsTxtTest() {
+    fun `test getRobotsTxt()`() {
         val robotsTxt = encarClient.getRobotsTxt()
         assertThat(robotsTxt).isNotNull()
-        println(robotsTxt)
+
+        val rules = crawlerUtil.parseRobotsTxtToRules(robotsTxt)
+
+        /* 차량 목록 조회 부분 */
+        assertThat(crawlerUtil.isPathAllowed(userAgent = "*", path = "/dc/dc_carsearchlist.do?carType=", rules)).isTrue
+        /* 차량 상세 조회 부분 */
+        assertThat(crawlerUtil.isPathAllowed(userAgent = "*", path = "/dc/dc_cardetailview.do?page_id=", rules)).isTrue
     }
 }
--- src/test/kotlin/devsy/toy/openfeign/KcarClientTest.kt
+++ src/test/kotlin/devsy/toy/openfeign/KcarClientTest.kt
@@ -1,18 +1,29 @@
 package devsy.toy.openfeign
 
+import devsy.toy.util.CrawlerUtil
 import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class KcarClientTest {
-    @Autowired
-    private lateinit var kCarClient: KCarClient
+class KcarClientTest(
+    @Autowired val kCarClient: KCarClient,
+    @Autowired val crawlerUtil: CrawlerUtil
+) {
+
     @Test
     fun getRobotsTxtTest() {
         val robotsTxt = kCarClient.getRobotsTxt()
         Assertions.assertThat(robotsTxt).isNotNull()
-        println(robotsTxt)
+
+        val rules = crawlerUtil.parseRobotsTxtToRules(robotsTxt)
+
+        /* 차량 목록 조회 */
+        assertThat(crawlerUtil.isPathAllowed(userAgent = "*", path = "/bc/search", rules)).isTrue
+        /* 차량 상세 조회 */
+        assertThat(crawlerUtil.isPathAllowed(userAgent = "*", path = "/bc/detail/carInfoDtl?i_sCarCd=EC61030844", rules)).isTrue
+
     }
 }
