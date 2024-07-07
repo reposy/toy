e628698f02fd744a6f7d0a6c292e9e76429fb2f3
syhan <hsykys0728@gmail.com>
2024-07-07 22:08:35 +0900

    add encar getVehicleList, getVehicleDetail

--- src/main/kotlin/devsy/toy/openfeign/EncarClient.kt
+++ src/main/kotlin/devsy/toy/openfeign/EncarClient.kt
@@ -2,10 +2,26 @@ package devsy.toy.openfeign
 
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 
 @FeignClient(name = "encarClient", url = "https://www.encar.com")
 interface EncarClient {
 
     @GetMapping("/robots.txt")
-    fun getRobotsTxt(): String
+    fun getRobotsTxt(
+        @RequestHeader("Content-Type") contentType: String = "application/json; charset=utf-8"
+    ): String
+
+    /* 차량 목록 조회 */
+    @GetMapping("/dc/dc_carsearchlist.do")
+    fun getVehicleList(
+        @RequestHeader("Content-Type") contentType: String = "application/json; charset=utf-8"
+    ): String
+
+    /* 차량 상세 조회 */
+    @GetMapping("/dc/dc_cardetailview.do?")
+    fun getVehicleDetail(
+        @RequestHeader("Content-Type") contentType: String = "application/json; charset=utf-8"
+    ): String
+
 }
